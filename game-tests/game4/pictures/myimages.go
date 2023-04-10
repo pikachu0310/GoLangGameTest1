4c3a4507579459ef2ddd7937244f7e95be9e4b80
pikachu0310 <sunnon@i.softbank.jp>
2023-04-10 14:30:33 +0900

    画像の追加

---
+++ game-tests/game4/pictures/myimages.go
@@ -0,0 +1,16 @@
+package myimages
+
+import (
+	_ "embed"
+)
+
+var (
+	//go:embed slime.png
+	SlimePng []byte
+	//go:embed weapon.png
+	WeaponPng []byte
+	//go:embed armor.png
+	ArmorPng []byte
+	//go:embed item.png
+	ItemPng []byte
+)
